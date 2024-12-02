27f0f0685d8d4618c7862c465f905d189e0c745f
GGueMMue <daehan121993@gmail.com>
2024-12-02 15:01:25 +0900

    post
    post

--- nodejs_day1/app.js
+++ nodejs_day1/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => 
+app.get('/', (req, res) => //주소 값과 익명함수 두 개의 파라메터
     {
         res.send('Welcome to My NodeJS Sevrer!!!')
     }
@@ -13,6 +13,13 @@ app.get('/', (req, res) =>
 app.get('/welcome', (req, res) => 
     {
         res.send('Welcome to welcome page')
+    }
+);
+
+app.post('/testPost', (req, res) => 
+    {
+        console.log(req.body.userID);
+        res.send(req.body.userID);
     }
 );
 
